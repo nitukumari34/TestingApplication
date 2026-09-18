0a168f5964b8d2044c7483c0fab1bda12003ef06
Nitu Kumari <nituspj032001@gmail.com>
2026-09-18 19:13:19 +0530

    feat(entity): add email and salary fields with Lombok annotations

--- src/main/java/com/TestingApp/entities/Employee.java
+++ src/main/java/com/TestingApp/entities/Employee.java
@@ -1,9 +1,6 @@
 package com.TestingApp.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Getter
@@ -15,5 +12,8 @@ public class Employee {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private  Long id;
+    @Column(unique = true)
+    private  String email;
     private  String name;
+    private  double salary;
 }
