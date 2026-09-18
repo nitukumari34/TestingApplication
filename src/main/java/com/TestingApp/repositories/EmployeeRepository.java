506523ee7675dcd48f7424ceaba6ce385d027253
Nitu Kumari <nituspj032001@gmail.com>
2026-09-18 19:14:01 +0530

    feat(repo): add findByEmail query method to EmployeeRepository

--- src/main/java/com/TestingApp/repositories/EmployeeRepository.java
+++ src/main/java/com/TestingApp/repositories/EmployeeRepository.java
@@ -2,6 +2,11 @@ package com.TestingApp.repositories;
 
 import com.TestingApp.entities.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+    List<Employee> findByEmail(String email);
 }
