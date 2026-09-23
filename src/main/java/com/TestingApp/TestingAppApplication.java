d91e528417591c09e0af6924bdef5903553811ef
Nitu Kumari <nituspj032001@gmail.com>
2026-09-23 14:01:13 +0530

    added spring profile

--- src/main/java/com/TestingApp/TestingAppApplication.java
+++ src/main/java/com/TestingApp/TestingAppApplication.java
@@ -1,14 +1,18 @@
 package com.TestingApp;
 
 import com.TestingApp.services.DataService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class TestingAppApplication  implements CommandLineRunner {
 
 	private final DataService dataService;
+	@Value("${my.variable}")
+	private  String myVariable;
 
     public TestingAppApplication(DataService dataService) {
         this.dataService = dataService;
@@ -19,6 +23,7 @@ public class TestingAppApplication  implements CommandLineRunner {
 	}
 	@Override
 	public  void run(String... args) throws  Exception{
+		System.out.println("My variable : " + myVariable);
 		System.out.println("The data is :"+ dataService.getData());
 
 	}
