79d5ef314a1b43456216a60df9c50785bbb18ea8
Nitu Kumari <nituspj032001@gmail.com>
2026-09-24 16:39:10 +0530

    refactor(app): comment out DataService injection in CommandLineRunner

--- src/main/java/com/TestingApp/TestingAppApplication.java
+++ src/main/java/com/TestingApp/TestingAppApplication.java
@@ -8,18 +8,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TestingAppApplication  implements CommandLineRunner {
 
-	private final DataService dataService;
-
-    public TestingAppApplication(DataService dataService) {
-        this.dataService = dataService;
-    }
+//	private final DataService dataService;
+//
+//    public TestingAppApplication(DataService dataService) {
+//        this.dataService = dataService;
+//    }
 
     public static void main(String[] args) {
 		SpringApplication.run(TestingAppApplication.class, args);
 	}
 	@Override
 	public  void run(String... args) throws  Exception{
-		System.out.println("The data is :"+ dataService.getData());
+//		System.out.println("The data is :"+ dataService.getData());
 
 	}
 
