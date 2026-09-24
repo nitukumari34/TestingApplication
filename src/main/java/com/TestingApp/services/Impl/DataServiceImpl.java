6cfbb42817eb421c8f01e32495fc8d0b178f0074
Nitu Kumari <nituspj032001@gmail.com>
2026-09-24 13:18:09 +0530

    fix env profile

--- src/main/java/com/TestingApp/services/Impl/DataServiceImpl.java
+++ src/main/java/com/TestingApp/services/Impl/DataServiceImpl.java
@@ -1,14 +1,18 @@
 package com.TestingApp.services.Impl;
 
 import com.TestingApp.services.DataService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 /**
- * Default implementation of {@link DataService} used when no specific Spring profile
- * (e.g., "dev" or "prod") is active. This bean ensures that the application can start
+ * Default implementation of {@link DataService} used when no specific Spring
+ * profile
+ * (e.g., "dev" or "prod") is active. This bean ensures that the application can
+ * start
  * without requiring a profile to be set.
  */
 @Service
+@Profile("default")
 public class DataServiceImpl implements DataService {
     @Override
     public String getData() {
