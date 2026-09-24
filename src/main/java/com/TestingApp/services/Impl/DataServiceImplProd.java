b58c9109076a13b56fec9e9ae1f0949cfd78faa8
Nitu Kumari <nituspj032001@gmail.com>
2026-09-24 16:39:27 +0530

    refactor(service): disable prod profile annotation on DataServiceImplProd

--- src/main/java/com/TestingApp/services/Impl/DataServiceImplProd.java
+++ src/main/java/com/TestingApp/services/Impl/DataServiceImplProd.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
-@Profile("prod")
+//@Profile("prod")
 public class DataServiceImplProd implements DataService {
     @Override
     public String getData() {
